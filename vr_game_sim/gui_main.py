0a8af4c9bbcbd250929af20c1d5f4d754fc509a8
Soto_JoseDeJesus <123611915+1104-Soto-JoseDeJesus@users.noreply.github.com>
2025-08-21 04:16:10 -0700

    Revert to solid color summary background

--- vr_game_sim/gui_main.py
+++ vr_game_sim/gui_main.py
@@ -946,26 +946,9 @@ class MainWindow(QtWidgets.QMainWindow):
         ]
         final_height = preview_pix.height() + sum(p.height() for p in hist_pixmaps)
         final_pix = QtGui.QPixmap(final_width, final_height)
-        final_pix.fill(QtCore.Qt.GlobalColor.transparent)
+        bg_color = self.palette().color(QtGui.QPalette.ColorRole.Window)
+        final_pix.fill(bg_color)
         painter = QtGui.QPainter(final_pix)
-
-        bg_path = os.path.join(
-            os.path.dirname(__file__),
-            "Reference Images",
-            "SummaryImageBackground_OMNI.png",
-        )
-        bg_pix = QtGui.QPixmap(bg_path)
-        if not bg_pix.isNull():
-            bg_scaled = bg_pix.scaled(
-                final_width,
-                final_height,
-                QtCore.Qt.AspectRatioMode.IgnoreAspectRatio,
-                QtCore.Qt.TransformationMode.SmoothTransformation,
-            )
-            painter.drawPixmap(0, 0, bg_scaled)
-        else:
-            bg_color = self.palette().color(QtGui.QPalette.ColorRole.Window)
-            painter.fillRect(final_pix.rect(), bg_color)
 
         x = (final_width - preview_pix.width()) // 2
         painter.drawPixmap(x, 0, preview_pix)

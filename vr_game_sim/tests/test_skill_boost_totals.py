4b41b31a0a23f49e139f8be1008e701db47ebb28
Soto_JoseDeJesus <123611915+1104-Soto-JoseDeJesus@users.noreply.github.com>
2025-10-25 03:01:46 -0700

    Track damage taken debuff kill contributions

--- vr_game_sim/tests/test_skill_boost_totals.py
+++ vr_game_sim/tests/test_skill_boost_totals.py
@@ -29,6 +29,52 @@ def test_damage_boost_tracks_units():
     assert attacker.skill_kill_boost_totals.get("boost_skill", 0) > 0
 
 
+def test_damage_taken_debuff_tracks_basic_attack_units():
+    atk_unit = Unit(unit_type="infantry", tier=5, initial_count=100)
+    dfd_unit = Unit(unit_type="infantry", tier=5, initial_count=100)
+    atk_unit.base_atk_stat = 1000
+    dfd_unit.base_def_stat = 1
+    attacker = Army(name="A", unit=atk_unit)
+    defender = Army(name="D", unit=dfd_unit)
+    sim = GameSimulator(attacker, defender)
+
+    effect_data = {
+        "name": "Expose",
+        "effect_type": EffectType.STAT_MOD,
+        "config": {"stat_to_mod": StatType.DAMAGE_TAKEN_MULTIPLIER},
+        "magnitude": 0.5,
+    }
+    eff = defender._create_and_add_single_effect(effect_data, "debuff_skill", attacker, defender)
+    defender.active_effects.append(eff)
+
+    sim._calculate_and_log_attack(attacker, defender, is_counter=False)
+
+    assert attacker.skill_kill_boost_totals.get("debuff_skill", 0) > 0
+
+
+def test_damage_taken_debuff_tracks_skill_units():
+    atk_unit = Unit(unit_type="infantry", tier=5, initial_count=100)
+    dfd_unit = Unit(unit_type="infantry", tier=5, initial_count=100)
+    atk_unit.base_atk_stat = 1000
+    dfd_unit.base_def_stat = 1
+    attacker = Army(name="A", unit=atk_unit)
+    defender = Army(name="D", unit=dfd_unit)
+    sim = GameSimulator(attacker, defender)
+
+    effect_data = {
+        "name": "Expose",
+        "effect_type": EffectType.STAT_MOD,
+        "config": {"stat_to_mod": StatType.DAMAGE_TAKEN_MULTIPLIER},
+        "magnitude": 0.5,
+    }
+    eff = defender._create_and_add_single_effect(effect_data, "skill_debuff", attacker, defender)
+    defender.active_effects.append(eff)
+
+    sim._calculate_generic_skill_damage(attacker, defender, damage_factor=200)
+
+    assert attacker.skill_kill_boost_totals.get("skill_debuff", 0) > 0
+
+
 def test_rage_boost_tracks_amount():
     unit = Unit(unit_type="infantry", tier=5, initial_count=100)
     army = Army(name="A", unit=unit)
